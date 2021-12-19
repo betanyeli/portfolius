27d03ffbb8caf4b89998475c5ccf52a51cb8873b
betanyeli <betangelii@gmail.com>
2021-12-19 13:58:06 -0300

    initial commit -m

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * Caution: Consider this file when using react-scripts
+ * 
+ * You may delete this file and its occurrences from the project filesystem if you are using GatsbyJS or NextJS version
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
